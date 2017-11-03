cbc304ac62635a685d7c27766887f8c5baa9cc35
Luca Sasselli <4eversax@gmail.com>
2017-11-03 14:10:36 +0100

    Missing file, moved to 1.7.1

---
+++ zero/src/main/java/com/lucasasselli/zero/Secrets.java
@@ -0,0 +1,10 @@
+package com.lucasasselli.zero;
+
+public class Secrets {
+    // This secret key allows to access PRO content
+    public final static String KEY_DOWNLOAD = "";
+
+    // Ad stuff (just to be sure)
+    final static String ADMOB_ID = "";
+    final static String ADMOB_INTERSTITIAL = "";
+}
